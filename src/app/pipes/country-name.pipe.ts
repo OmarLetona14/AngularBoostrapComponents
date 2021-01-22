57d35db5b4c8c8d3787167012a10d8f2219e3b48
Omar Letona <omarletonaf@gmail.com>
2021-01-22 14:02:44 -0600

    Fixed search pipe

--- src/app/pipes/country-name.pipe.ts
+++ src/app/pipes/country-name.pipe.ts
@@ -8,6 +8,7 @@ export class CountryNamePipe implements PipeTransform {
 
   transform(value: any, arg: any): any {
     const results = [];
+    if(arg==''){return value}
     for (const country of value){
       console.log(`Country: ${country.name}`);
       if ( country.name.toLowerCase().indexOf(arg.toLowerCase()) > -1){
